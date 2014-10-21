e3ac2c6eaa71845a9277a0325c331a984e4e6868
Roman Jurkov <winfinit@gmail.com>
2014-10-21 13:14:39 -0400

    changed interface and switched to jquery plugin

--- CardReader.js
+++ CardReader.js
@@ -6,140 +6,155 @@
  * Licensed under the MIT license (see LICENSE for details)
  */
 
-var CardReader = function (error_start, track_start, track_end, timeout) {
-	this.error_start = error_start || "é";
-	this.track_start = track_start || "%";
-	this.track_end = track_end || "_";
-	this.timeout = timeout || 100;
-	
-	this.error_start = this.error_start.charCodeAt(0);
-	this.track_start = this.track_start.charCodeAt(0);
-	this.track_end = this.track_end.charCodeAt(0);
-	
-	this.started = false;
-	this.finished = false;
-	this.isError = false;
-	this.input = "";
-	this.timer = undefined;
-	this.callbacks = [];
-	this.errbacks = [];
-	this.validators = [];
-	this.isDispatching = false;
-};
+(function(window, $) {
+	var CardReader = function(elem, options, callback) {
+		this.elem = elem;
+		this.$elem = $(elem);
+		this.options = options;
+		this.metadata = this.$elem.data('cardreader');
+		this.callback = callback;
 
-CardReader.prototype = {
-	dispatch: function (data, isError) {
-		if (!isError) {
-			for (var cb in this.validators) {
-				if (!this.validators[cb](data)) {
-					isError = true;
-					break;
-				}
-			}
-		}
-	
-		if (this.isDispatching) {
-			if (isError) {
-				console.log("Immediate error!");
-				return;
-			} else {
-				clearTimeout(this.isDispatching);
-			}
-		}
-	
-		var reader = this;
-	
-		this.isDispatching = setTimeout(function () {
-			console.log("Error timeout cleared");
-			reader.isDispatching = false;
-		}, 200);
-	
-		if (isError) {
-			for (var cb in this.errbacks) {
-				this.errbacks[cb](this.input);
-			}
-		} else {
-			for (var cb in this.callbacks) {
-				this.callbacks[cb](this.input);
-			}
-		}
-	},
-	
-	readObserver: function (e) {
-		var ob = this;
-		
-		if (!this.started && (e.which === this.track_start || e.which === this.error_start)) {
-			e.stopImmediatePropagation();
-			e.preventDefault();
-		
-			this.started = true;
-			this.isError = e.which === this.error_start;
-		
-			this.timer = setTimeout(function () {
-				ob.started = false;
-				ob.finished = false;
-				ob.isError = false;
-				ob.input = "";
-			}, this.timeout);
-		} else if (this.started && e.which === this.track_end) {
-			e.stopImmediatePropagation();
-			e.preventDefault();
-		
-			this.finished = true;
-		
-			clearTimeout(this.timer);
-			this.timer = setTimeout(function () {
-				ob.started = false;
-				ob.finished = false;
-				ob.isError = false;
-				ob.input = "";
-			}, this.timeout);
-		} else if (this.started && this.finished && e.which === 13) {
-			e.stopImmediatePropagation();
-			e.preventDefault();
-		
-			this.dispatch(this.input, this.isError);
+	};
+
+	CardReader.prototype = {
+
+		init: function() {
+			this.config = $.extend({}, this.defaults, this.options, this.metadata);
+
+			this.config.error_start = this.config.error_start.charCodeAt(0);
+			this.config.track_start = this.config.track_start.charCodeAt(0);
+			this.config.track_end = this.config.track_end.charCodeAt(0);
 
 			this.started = false;
 			this.finished = false;
 			this.isError = false;
 			this.input = "";
-		
-			clearTimeout(this.timer);
-		
-		} else if (this.started) {
-			e.stopImmediatePropagation();
-			e.preventDefault();
-		
-			this.input += String.fromCharCode(e.which);
-		
-			clearTimeout(this.timer);
-			this.timer = setTimeout(function () {
-				ob.started = false;
-				ob.finished = false;
-				ob.isError = false;
-				ob.input = "";
-			}, this.timeout);
+			this.timer = undefined;
+			this.isDispatching = false;
+
+			var obj = this;
+			this.$elem.keypress(function(e) {
+				CardReader.prototype.readObserver.apply(obj, arguments);
+			});
+
+			return this;
+		},
+		defaults: {
+			validators: [],
+			error_start: "é",
+			track_start: "%",
+			track_end: "_",
+			timeout: 100
+		},
+		dispatch: function(data, isError) {
+			if (!isError) {
+				for (var cb in this.config.validators) {
+					if (!this.config.validators[cb](data)) {
+						isError = true;
+						break;
+					}
+				}
+			}
+
+			if (this.isDispatching) {
+				if (isError) {
+					console.log("Immediate error!");
+					return;
+				} else {
+					clearTimeout(this.isDispatching);
+				}
+			}
+
+			var reader = this;
+
+			this.isDispatching = setTimeout(function() {
+				reader.isDispatching = false;
+			}, 200);
+
+			if (isError) {
+				console.log("error");
+
+				this.callback(true, this.input);
+			} else {
+				console.log("success");
+				this.callback(false, this.input);
+			}
+		},
+
+
+
+		readObserver: function(e) {
+			var ob = this;
+			if (!this.started && (e.which === this.config.track_start || e.which === this.config.error_start)) {
+				e.stopImmediatePropagation();
+				e.preventDefault();
+
+				this.input += String.fromCharCode(e.which);
+				this.started = true;
+				this.isError = e.which === this.error_start;
+
+				this.timer = setTimeout(function() {
+					ob.started = false;
+					ob.finished = false;
+					ob.isError = false;
+					ob.input = "";
+				}, this.config.timeout);
+			} else if (this.started && e.which === this.config.track_end) {
+				e.stopImmediatePropagation();
+				e.preventDefault();
+
+				this.input += String.fromCharCode(e.which);
+
+				this.finished = true;
+				clearTimeout(this.timer);
+				this.config.timer = setTimeout(function() {
+					ob.started = false;
+					ob.finished = false;
+					ob.isError = false;
+					ob.input = "";
+				}, this.config.timeout);
+			} else if (this.started && this.finished && e.which === 13) {
+				e.stopImmediatePropagation();
+				e.preventDefault();
+				this.dispatch(this.input, this.isError);
+
+				this.started = false;
+				this.finished = false;
+				this.isError = false;
+				this.input = "";
+
+				clearTimeout(this.timer);
+
+			} else if (this.started) {
+				e.stopImmediatePropagation();
+				e.preventDefault();
+				this.input += String.fromCharCode(e.which);
+
+				clearTimeout(this.timer);
+				this.timer = setTimeout(function() {
+					ob.started = false;
+					ob.finished = false;
+					ob.isError = false;
+					ob.input = "";
+				}, this.config.timeout);
+			}
+		},
+
+
+		validate: function(validator) {
+			this.config.validators.push(validator);
 		}
-	},
-	
-	observe: function (element) {
-		var func = this;
-	
-		$(element).keypress(function (e) {
-			CardReader.prototype.readObserver.apply(func, arguments);
+
+	};
+
+	CardReader.defaults = Plugin.prototype.defaults;
+
+	$.fn.cardReader = function(options, callback) {
+		return this.each(function() {
+			new CardReader(this, options, callback).init();
 		});
-	},
-	
-	validate: function (validator) {
-		this.validators.push(validator);
-	},
-	
-	cardRead: function (callback) {
-		this.callbacks.push(callback);
-	},
-	
-	cardError: function (errback) {
-		this.errbacks.push(errback);
-	},
-}
+	};
+
+	window.CardReader = CardReader;
+
+})(window, jQuery);
